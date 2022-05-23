69f4900fa4a81e5f547fe64a678e79787145dfe8
Tim-Nicholson <t.r.nicho@gmail.com>
2022-05-23 14:55:47 -0400

    working base code

--- digipets/main_app/views.py
+++ digipets/main_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def landing(request):
+  return render(request, 'landing.html')
+
+def signup(request):
+  error_message=''
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
