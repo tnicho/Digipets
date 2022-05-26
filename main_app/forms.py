34b56b6a0c9a90361b6492617bcf30ad38d53170
zainamirza <zain_amir@outlook.com>
2022-05-26 10:06:52 -0400

    more styling changes

--- main_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Digipet
-
-class DigipetsForm(ModelForm):
-  class Meta:
-    model = Digipet
-    fields = ['name', 'species', 'birthday', 'personality']
