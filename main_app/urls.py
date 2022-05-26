552814055892643a67952f90475f86511c4423c5
user <user@users-MacBook-Pro.local>
2022-05-26 10:41:37 -0400

    updating code

---
+++ main_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+	path('', views.home, name='home'),
+	path('digipets/', views.digipets_index, name='index'),
+	path('landing/', views.landing, name='landing'),
+	path('digipets/<int:digipet_id>/', views.digipets_detail, name='detail'),
+	path('digipets/<int:pk>/update/', views.DigipetUpdate.as_view(), name='digipets_update'),
+	#path('digipets/<int:digipet_id>/update/', views.digipets_update, name='digipets_update'),
+	path('digipets/<int:pk>/feed/', views.digipets_feed, name='digipets_feed'),
+  path('accounts/signup/', views.signup, name='signup'),
+  path('digipets/create/', views.DigipetCreate.as_view(), name='digipets_create'),
+  path('digipets/<int:pk>/delete/', views.DigipetDelete.as_view(), name='digipets_delete'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.views.generic.edit import CreateView
+from .models import Digipet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import DigipetsForm
+from django.forms import ModelForm
+from django.views.generic.edit import UpdateView, DeleteView
+# Create your views here.
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def landing(request):
+  return render(request, 'landing.html')
+
+@login_required
+def digipets_update(request, digipet_id):
+  return render(request, 'digipets/update.html')
+
+@login_required
+def digipets_detail(request, digipet_id):
+  digipet = Digipet.objects.get(id=digipet_id)
+  return render(request, 'digipets/detail.html', {'digipet': digipet})
+
+@login_required
+def digipets_index(request):
+  digipets = Digipet.objects.filter(user=request.user)
+  return render(request, 'digipets/index.html', {'digipets': digipets})
+
+def signup(request):
+  error_message=''
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+def digipets_feed (request, digipet_id):
+  Digipet.objects.get(id=digipet_id).feed
+  return redirect(request, 'digipets_detail', digipet_id = digipet_id)
+  
+class DigipetCreate(LoginRequiredMixin, CreateView):
+  model = Digipet
+  #fields = ['name', 'species', 'personality', 'birthday', 'image']
+  fields = ['name', 'personality', 'birthday', 'species', 'image']
+  # digipets_form = DigipetsForm()
+  #success_url = '/digipets/'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    # taking the user and assigning them to the form instance
+    return super().form_valid(form)
+
+
+class DigipetUpdate(LoginRequiredMixin, UpdateView):
+  model = Digipet
+  fields = ['name', 'personality']
+
+class DigipetDelete(LoginRequiredMixin, DeleteView):
+  model = Digipet
+  success_url = '/digipets/'
