19fa09483e85e4bcfa55f41ee5d91fb4fd320c73
Tim-Nicholson <t.r.nicho@gmail.com>
2022-05-26 12:33:14 -0400

    sign up fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('digipets/<int:pk>/update/', views.DigipetUpdate.as_view(), name='digipets_update'),
 	#path('digipets/<int:digipet_id>/update/', views.digipets_update, name='digipets_update'),
 	path('digipets/<int:pk>/feed/', views.digipets_feed, name='digipets_feed'),
+	path('accounts/', include('django.contrib.auth.urls')),
   path('accounts/signup/', views.signup, name='signup'),
   path('digipets/create/', views.DigipetCreate.as_view(), name='digipets_create'),
   path('digipets/<int:pk>/delete/', views.DigipetDelete.as_view(), name='digipets_delete'),
